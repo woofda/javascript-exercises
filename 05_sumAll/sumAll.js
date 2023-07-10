7e36d14f0519b35b485b1fdaae765ee8868893f0
woofda <130379195+woofda@users.noreply.github.com>
2023-07-10 08:37:55 -0500

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if(typeof(num1) !== "number" || typeof(num2) !== "number"){
+        return "ERROR";
+    }
 
+    if(num1 < 0 || num2 < 0){
+        return "ERROR";
+    }
+
+    if(num2 < num1){
+        let temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+
+    let output = 0;
+    for(let i = num1; i <= num2; i++){
+        output += i;
+    }
+    return output;
 };
 
 // Do not edit below this line
