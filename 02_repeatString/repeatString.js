7483aaba8cf590ad2d4e8a787f00eb7ff1e842dc
woofda <130379195+woofda@users.noreply.github.com>
2023-07-09 20:17:43 -0500

    Complete exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if(num < 0) {
+        return "ERROR";
+    }
+    let output = '';
+    for(; num > 0; num--){
+        output += str;
+    }
+    return output;
 };
 
 // Do not edit below this line
