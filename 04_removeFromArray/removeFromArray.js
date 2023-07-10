2667964f8031e98ac9e17fb692ccdce9304d6811
woofda <130379195+woofda@users.noreply.github.com>
2023-07-10 08:24:28 -0500

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...restOfArgs) {
+    for(let i = 0; i < array.length; i++){
+        for(item of restOfArgs){
+            if(array[i] === item){
+                array.splice(i, 1);
+                i--;
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
