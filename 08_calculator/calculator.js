beb09bce769648b5004117dfe6ca96d0c23dd053
woofda <130379195+woofda@users.noreply.github.com>
2023-07-22 18:17:29 -0500

    Complete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(...nums) {
+  return nums.reduce( (total, num) => total += num, 0);
 };
 
-const subtract = function() {
-	
+const subtract = function(a = 0, b = 0) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce( (total, num) => total += num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(...nums) {
+  return nums.reduce( (total, num) => total *= num, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  output = 1;
+	for(let i = 1; i <= num; i++) {
+    output *= i;
+  }
+  return output;
 };
 
 // Do not edit below this line
