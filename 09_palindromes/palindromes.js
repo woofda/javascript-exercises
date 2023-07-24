422b9319217b1f5246ed7171309d6580624c85e8
woofda <130379195+woofda@users.noreply.github.com>
2023-07-24 10:37:51 -0500

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let initial = word.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").replace(/\s+/g, '').split('');
+    let reversed = [...initial].reverse();
+    for(let i = 0; i < initial.length; i++) {
+        if(initial[i] !== reversed[i]) return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
