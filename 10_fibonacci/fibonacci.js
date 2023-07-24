2a7b395f0d14c6a7220786466c77ec6e52a9087f
woofda <130379195+woofda@users.noreply.github.com>
2023-07-24 10:48:39 -0500

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,8 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    let parsedNum = parseInt(number);
+    if(isNaN(parsedNum) || parsedNum < 1) return "OOPS";
+    if(parsedNum === 1 || parsedNum === 2) return 1;
+    else return (fibonacci(parsedNum - 1) + fibonacci(parsedNum - 2));
 };
 
 // Do not edit below this line
