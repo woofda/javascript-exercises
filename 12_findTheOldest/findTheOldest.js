4d45077f32036bbdb4f2d892cf952fabcf729765
woofda <130379195+woofda@users.noreply.github.com>
2023-07-24 11:20:55 -0500

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
 
+    function getAge(person){
+        let age;
+        let currentYear = new Date();
+
+        if('yearOfDeath' in person){
+            age = person.yearOfDeath - person.yearOfBirth;
+        } else {
+            age = currentYear.getFullYear() - person.yearOfBirth;
+        }
+        return age;
+    }
+
+    return array.reduce((oldestPerson, newPerson) => 
+        (getAge(newPerson) > getAge(oldestPerson)) ? newPerson : oldestPerson);
 };
 
 // Do not edit below this line
